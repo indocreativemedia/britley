5b5a738f152519cbe0d6cecf4183bc28ecb144ce
indocreativemedia <admin@indocreativemedia.com>
2025-08-16 14:18:04 +0700

    Display post excerpt if exist & fix "Continue reading" link

--- template-parts/content.php
+++ template-parts/content.php
@@ -32,27 +32,26 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'britley' ),
-					array(
-						'span' => array(
-							'class' => array(),
+			if ( has_excerpt() ) {
+		    the_excerpt();
+			} else {
+				the_content(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Post title. Only visible to screen readers. */
+							__( 'Continue reading %s', 'britley' ),
+							array()
 						),
+						'<span class="screen-reader-text d-block">' . wp_kses_post( get_the_title() ) . '</span>'
 					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="clearfix"></div><div class="page-links">' . esc_html__( 'Pages:', 'britley' ),
-				'after'  => '</div>',
-			)
-		);
+				);
+				wp_link_pages(
+					array(
+						'before' => '<div class="clearfix"></div><div class="page-links">' . esc_html__( 'Pages:', 'britley' ),
+						'after'  => '</div>',
+					)
+				);
+			}
 		?>
 	</div><!-- .entry-content -->
 
